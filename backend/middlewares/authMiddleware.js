2a2c71b042f7d48cc4dc3df7dcda2ac1e3de7d10
Quentin Feuvrais <feuvrais.quentin@gmail.com>
2023-01-12 16:49:07 +0100

    authMiddleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,25 +1,18 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
 
 module.exports = function (req, res, next) {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const token = req.headers.authorization.split(" ")[1];
     if (!token) {
-      return res.status(403).json({ message: "You need to login first" });
       return res.status(403).json({ message: "You need to login first" });
     }
     const decodedData = jwt.verify(token, process.env.SECRET);
-    console.log(decodedData)
+    console.log(decodedData);
     req.user = decodedData;
     next();
   } catch (e) {
     console.log(e);
     return res.status(403).json({ message: "You need to login first" });
-    console.log(e);
-    return res.status(403).json({ message: "You need to login first" });
   }
-};
 };
