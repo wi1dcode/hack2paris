e932fc027160402b1f99140a56ba6d5287526950
unknown <magosdk@icloud.com>
2023-01-12 14:27:56 +0100

    started list component

--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import TextField from "@mui/material/TextField"
+import Button from "@mui/material/Button"
 
 function List() {
-  return <div>List</div>;
+  const [products, setProducts] = useState(null)
+  const [where, setWhere] = useState("")
+  const [when, setWhen] = useState("")
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/products").then((res) => {
+      setProducts(res)
+    })
+  }, [products])
+
+  if (!post) return null;
+
+  return <div>List</div>
 }
 
-export default List;
+export default List
