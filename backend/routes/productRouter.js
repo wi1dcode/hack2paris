df3c3fb9952637535246ec603e13a791f77cebe4
unknown <magosdk@icloud.com>
2023-01-13 10:21:00 +0100

    update

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,14 +1,13 @@
-const Router = require("express");
-const router = new Router();
-const controller = require("../controllers/productController");
-const { check } = require("express-validator");
-const authMiddleware = require("../middlewares/authMiddleware");
+const Router = require("express")
+const router = new Router()
+const controller = require("../controllers/productController")
+const { check } = require("express-validator")
+const authMiddleware = require("../middlewares/authMiddleware")
 
-// router.use(authMiddleware);
-
-router.post("/", controller.postProduct);
+router.get("/", controller.getProducts)
+router.get("/:id", controller.getProductById)
+router.use(authMiddleware)
+router.post("/", controller.postProduct)
 router.post("/:id/rents", controller.rentProduct)
-router.get("/", controller.getProducts);
-router.get("/:id", controller.getProductById);
 
 module.exports = router
