41c7b7145f8232d6abb0e5419201c57200022ba0
Jordann <jordannlegal.contact@gmail.com>
2023-01-13 10:34:41 +0100

    link research with list

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Button from '@mui/material/Button';
-import MenuIcon from '@mui/icons-material/Menu';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import Button from "@mui/material/Button";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
 //import AnimationIcon from '@mui/icons-material/Animation';
-import OutboundIcon from '@mui/icons-material/Outbound';
+import OutboundIcon from "@mui/icons-material/Outbound";
 import ConnexionPanel from "./ConnexionPanel";
 
-function handlerMenu(){
-  let panelMenu = document.querySelector('.header__panel');
+function handlerMenu() {
+  let panelMenu = document.querySelector(".header__panel");
   panelMenu.classList.toggle("opened");
 }
 
@@ -23,13 +23,13 @@ function Header() {
         </span>
       </Link>
       <Button
-        className="header__menu" 
-        variant="contained" 
-        startIcon={<AccountBoxIcon className="isNoMobile" />} 
+        className="header__menu"
+        variant="contained"
+        startIcon={<AccountBoxIcon className="isNoMobile" />}
         endIcon={<MenuIcon />}
         onClick={handlerMenu}
-        >
-          <span className="srOnly">Menu Connexion</span>
+      >
+        <span className="srOnly">Menu Connexion</span>
       </Button>
       <ConnexionPanel />
     </header>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,63 +1,65 @@
-import React, { useContext, useState } from "react"
-import TextField from "@mui/material/TextField"
-import Button from "@mui/material/Button"
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
-import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker"
-import ResearchInfosContext from "../contexts/ResearchInfos"
-
-import dayjs from "dayjs"
+import React, { useContext, useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import ResearchInfosContext from "../contexts/ResearchInfos";
+import { Link /* useNavigate */ } from "react-router-dom";
 
 function Home() {
-  const { whereInput, setWhereInput, setDateFormated } =
-    useContext(ResearchInfosContext)
+  const {
+    whereInput,
+    setWhereInput,
+    setDateFormated,
+    dateFormated,
+    setDateRaw,
+    dateRaw,
+    onSubmitSearchProducts,
+  } = useContext(ResearchInfosContext);
 
-  const [dateRaw, setRawDate] = useState(dayjs("2023-01-12T09:00:00"))
+  /*   const navigate = useNavigate(); */
 
   const handleChange = (newValue) => {
-    setRawDate(newValue)
-  }
-
-  async function onSubmitSearchProducts(event) {
-    event.preventDefault()
+    setDateRaw(newValue);
     setDateFormated(
-      `${dateRaw.$D < 10 ? "0" : ""}${dateRaw.$D}/${
-        dateRaw.$M < 10 ? "0" : ""
-      }${dateRaw.$M + 1}/${dateRaw.$y}`
-    )
-  }
+      `${newValue.$D < 10 ? "0" : ""}${newValue.$D}/${
+        newValue.$M < 10 ? "0" : ""
+      }${newValue.$M + 1}/${newValue.$y}`
+    );
+  };
 
+  console.log(dateFormated);
+  console.log(whereInput);
   return (
     <main className="main homepage">
       <form className="form form--only" onSubmit={onSubmitSearchProducts}>
-        <div>
-          <TextField
+        <TextField
+          className="form__fieldset"
+          helperText="Type the place where you want to rent"
+          label="Where are you?"
+          id="demo-helper-text-aligned"
+          value={whereInput}
+          onInput={(e) => setWhereInput(e.target.value)}
+        />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DesktopDatePicker
             className="form__fieldset"
-            helperText="Type the place where you want to rent"
-            label="Where are you?"
-            id="demo-helper-text-aligned"
-            value={whereInput}
-            onInput={(e) => setWhereInput(e.target.value)}
+            label="Choose a date"
+            inputFormat="DD/MM/YYYY"
+            value={dateRaw}
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} />}
           />
-        </div>
-        <div>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DesktopDatePicker
-              className="form__fieldset"
-              label="Choose a date"
-              inputFormat="DD/MM/YYYY"
-              value={dateRaw}
-              onChange={handleChange}
-              renderInput={(params) => <TextField {...params} />}
-            />
-          </LocalizationProvider>
-        </div>
-        <Button className="acces-btn" variant="contained" type="submit">
-          Search
-        </Button>
+        </LocalizationProvider>
+        <Link to="/list">
+          <Button className="acces-btn" variant="contained" type="submit">
+            Search
+          </Button>
+        </Link>
       </form>
     </main>
-  )
+  );
 }
 
-export default Home
+export default Home;
